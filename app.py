b84e2e12203e3e03046ec95319632842426b24e1
Максим Румянцев <drunkwolfs1@gmail.com>
2024-10-31 17:12:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def render_image():
     return send_file(BytesIO(img), mimetype='image/png', as_attachment=True, download_name='output_image.png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
